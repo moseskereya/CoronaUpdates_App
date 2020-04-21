6889f5ccc631b5aa8e9a83f7d742401902132c3c
Moses <lazaromoses77@gmail.com>
2020-04-21 16:03:10 +0300

    Updates

---
+++ precache-manifest.162518b49288a3a973ec223f9f3d3537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bd0704109d223d495e8485eca37d6c",
+    "url": "/CoronaUpdates_App/index.html"
+  },
+  {
+    "revision": "04c0db1505906f7afb15",
+    "url": "/CoronaUpdates_App/static/css/main.71bff6ff.chunk.css"
+  },
+  {
+    "revision": "adefde0bb6b668e1205d",
+    "url": "/CoronaUpdates_App/static/js/2.57256342.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/CoronaUpdates_App/static/js/2.57256342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c0db1505906f7afb15",
+    "url": "/CoronaUpdates_App/static/js/main.c883ada1.chunk.js"
+  },
+  {
+    "revision": "fd65ed8c2e5657e6daba",
+    "url": "/CoronaUpdates_App/static/js/runtime-main.646f736e.js"
+  },
+  {
+    "revision": "875c9af6b8e1389d276b4566bb608708",
+    "url": "/CoronaUpdates_App/static/media/Moses.875c9af6.jpeg"
+  }
+]);
